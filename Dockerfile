72220eb25e9304a06c6fad75c1f2e6a3aa578182
mnsrulz <manishrawat4u@gmail.com>
2023-11-04 16:15:08 +0000

    using netlify api for cache links

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,12 @@
 FROM node:20 AS builder
-# RUN apk add --update npm
 WORKDIR /app
 COPY package*.json ./
 RUN npm ci
-# RUN npm i -g @vercel/ncc
 COPY . .
-# RUN ncc build src/server.ts --minify -o dist --target es2021
 
 RUN npm run build
 
 FROM node:20-alpine
-# RUN apk add --update nodejs
 WORKDIR /app
 
 COPY ./views ./views
--- src/apiClient.ts
+++ src/apiClient.ts
@@ -4,8 +4,8 @@ import config from './config.js';
 const instance = got.extend({ prefixUrl: config.linksApiUrl });
 interface linksResponse {
     count: number
-    results: {
-        _id: string,
+    items: {
+        id: string,
         contentType: string,
         lastModified: string,
         status: string,
@@ -16,13 +16,14 @@ interface linksResponse {
     }[]
 }
 export const getLinks = async (imdbId: string, size: number) => {
-    const u = await instance(`api/links?imdbId=${imdbId}&pageSize=100&size=${size}&sortField=speedRank&sortOrder=desc`)
+    log.info(`requesting getLinks for imdbId: '${imdbId}' with size: '${size}'`);
+    const u = await instance(`api/links?imdbId=${imdbId}&per_page=100&size=${size}`)
         .json<linksResponse>();
-    return u.results.filter(x => x.status === 'Valid');
+    return u.items.filter(x => x.status === 'Valid');
 }
 
 export const requestRefresh = async (docId: string) => {
-    const urlPath = `api/links/refresh/${docId}`;
+    const urlPath = `api/links/${docId}/refresh`;
     try {
         log.info(`requesting refresh for docId: ${docId}`);
         await instance.post(urlPath);
